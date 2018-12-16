d42f926db1ae4de246f41504beed7925718af8f6
Parker Holladay <parker-holladay@pluralsight.com>
2018-12-16 11:06:32 -0700

    Mess around with f# some more

--- dotnet/fsharp/Program.fs
+++ dotnet/fsharp/Program.fs
@@ -1,6 +1,14 @@
-﻿open System
+﻿let rec factorial n =
+  if n < 2 then
+    1
+  else
+    n * factorial (n - 1)
+
+let helloWorld x = 
+  printfn "Hello World from F#! Factorial: %d" (factorial x)
 
 [<EntryPoint>]
 let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+  let number = argv.[0] |> int
+  helloWorld number
+  0 // return an integer exit code
