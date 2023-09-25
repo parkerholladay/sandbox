2cc5ab6f072977601d6191767d9e169ec4732928
Parker Holladay <parker.holladay@gmail.com>
2023-09-25 13:29:22 -0600

    Update node server for POST requests with data

--- node/src/server.js
+++ node/src/server.js
@@ -1,15 +1,31 @@
 const http = require('http')
 
 const port = 9001
+const server = http.createServer()
 
-const server = http.createServer((req, res) => {
+server.on('request', (req, res) => {
   console.log("req.method:", req.method)
   console.log("req.url:", req.url)
   console.log("req.headers:", req.headers)
-  console.log("req.data:", req.data)
 
-  res.statusCode = 200
-  res.end(JSON.stringify({ foo: 'bar' }))
+  let request = ''
+  req.on('data', chunk => request += chunk)
+
+  req.on('end', () => {
+    console.log("req.body:", request)
+    const body = JSON.parse(request ? request : '{}')
+
+    body.method = req.method
+    body.url = req.url
+
+    res.writeHead(200, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify(body))
+  })
+
+  req.on('err', () => {
+    res.writeHead(500, { 'Content-Type': 'application/json' })
+    res.end(JSON.stringify({ error: 'You bwoke it!' }))
+  })
 })
 
-server.listen(port, () => console.log(`Listening on port ${port}`))
+server.listen(port, () => console.info(`🚀 Listening on port ${port}`))
